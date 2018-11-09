27bf97d8aa8ae6047b92c504d54cbaa91f02b3a7
Jean Némard <17novembre.info@mail.tld>
2018-11-09 19:05:23 +0100

    feat: add text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>Page en construction</h1>
+    <p>Le 17 novembre est le 321e jour de l'année du calendrier grégorien, le 322e en cas d'année bissextile. Il reste 44 jours avant la fin de l'année. C'était généralement le 27e jour du mois de brumaire dans le calendrier républicain français, officiellement dénommé jour du macjonc. </p>
   </div>
 </template>
