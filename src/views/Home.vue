73cdf465dad10668dec977cd88b295b01e168ddc
Jean Némard <17novembre.info@mail.tld>
2018-11-09 16:22:45 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SiteTitle msg="17Novembre.info"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SiteTitle from '@/components/SiteTitle.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    SiteTitle
   }
 }
 </script>
